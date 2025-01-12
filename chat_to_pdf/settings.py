1659bf5253458b771e7ff237dc81d6843252c2a0
manish gaud <maneeshgaudg@gmail.com>
2025-01-12 15:27:28 +0530

    added

--- chat_to_pdf/settings.py
+++ chat_to_pdf/settings.py
@@ -9,9 +9,12 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+from dotenv import load_dotenv
 from pathlib import Path
 import os
+
+# import load env
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +80,12 @@ WSGI_APPLICATION = 'chat_to_pdf.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv("db_name"),
+        'USER': os.getenv('db_user_name'),
+        'PASSWORD': os.getenv('db_password') ,
+        'HOST': os.getenv('db_host'),
+        'PORT': os.getenv('db_port')
     }
 }
 
